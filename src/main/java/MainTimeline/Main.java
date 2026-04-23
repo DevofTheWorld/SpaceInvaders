03ec013e1a12768a6fe48c9ee03fba744974d25a
Ben Dol <bendoljemsbond@gmail.com>
2026-04-23 19:23:11 +0800

    Added title in menu

--- src/main/java/MainTimeline/Main.java
+++ src/main/java/MainTimeline/Main.java
@@ -18,22 +18,31 @@ public class Main extends Application {
 
         VBox menuBox = new VBox();
         menuBox.setAlignment(Pos.CENTER);
-        menuBox.setSpacing(15);
+        menuBox.setSpacing(30);
 
+        //title for menu
+        Label titleLabel = new Label("SPACE INVADERS");
+        titleLabel.getStyleClass().add("game-title");
+        
+
+
+        
         Image bgImg = new Image(getClass().getResource("/animatedbackground.gif").toExternalForm());
         ImageView background = new ImageView(bgImg);
         background.setFitWidth(720);
         background.setFitHeight(720);
-
+        
         StackPane menuRoot = new StackPane(background, menuBox);
         Scene menuScene = new Scene(menuRoot, 720, 720);
 
+        //code connecting to CSS
+        menuScene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        
         Button startBtn = new Button("Start Game");
         Button aboutBtn = new Button("About");
         Button instBtn = new Button("Instructions");
         Button exitBtn = new Button("Exit");
-
-        menuBox.getChildren().addAll(startBtn, aboutBtn, instBtn, exitBtn);
+        
 
         menuUI ui = new menuUI();
 
@@ -59,6 +68,8 @@ public class Main extends Application {
                 control.setup(gameScene);
             });
         });
+
+        menuBox.getChildren().addAll(titleLabel, startBtn, aboutBtn, instBtn, exitBtn);
 
         aboutBtn.setOnAction(e -> stage.setScene(ui.abtBtn(stage, menuScene)));
         instBtn.setOnAction(e -> stage.setScene(ui.instructionBtn(stage, menuScene)));
